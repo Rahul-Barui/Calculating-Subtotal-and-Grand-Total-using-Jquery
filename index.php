40ec7ef1608f99bdd6b0882a66053e132e5855b2
Rahul Barui <40061928+Rahul-Barui@users.noreply.github.com>
2019-06-08 15:56:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">	
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <title>Subtotal & Grandtotal | Rahul Barui ( Php Developer ) </title>
 	
 	<!-- Use The Latest JQuery Library -->
-	<script type="text/javascript" src="jquery-3.4.1.min.js"></script>
+	
 	<script type="text/javascript">
             $(document).ready(function() {
 				$('.marks1,.subtotal,.marks2,.subtotal2,.marks3,.subtotal3').keyup(function () {
